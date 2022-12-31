6187def41592c855baa5f8dc8fa9c28be5537b76
Sebastian Kürten <sebastian@topobyte.de>
2022-12-31 12:10:39 +0100

    Implement JVM-based DEB packager

---
+++ pinpit/src/test/kotlin/de/mobanisto/pinpit/validation/deb/AbstractDebPackager.kt
@@ -0,0 +1,148 @@
+/*
+ * Copyright 2022 Mobanisto UG (haftungsbeschraenkt) and respective authors and developers.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
+ */
+
+package de.mobanisto.pinpit.validation.deb
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.io.Writer
+import java.nio.file.FileVisitResult
+import java.nio.file.Files
+import java.nio.file.Files.createDirectories
+import java.nio.file.Files.newBufferedWriter
+import java.nio.file.Files.setPosixFilePermissions
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.nio.file.SimpleFileVisitor
+import java.nio.file.attribute.BasicFileAttributes
+import java.nio.file.attribute.PosixFilePermission
+import java.nio.file.attribute.PosixFilePermissions
+import java.nio.file.attribute.PosixFilePermissions.asFileAttribute
+import kotlin.io.path.createDirectories
+
+abstract class AbstractDebPackager constructor(
+    workingDir: Path,
+    private val packageName: String,
+    private val linuxPackageName: String,
+    private val packageVersion: String,
+    private val appCategory: String,
+    private val packageVendor: String,
+    private val debMaintainer: String,
+    private val packageDescription: String,
+    private val depends: List<String>,
+    private val debCopyright: Path?,
+    private val debLauncher: Path?,
+    private val debPreInst: Path?,
+    private val debPostInst: Path?,
+    private val debPreRm: Path?,
+    private val debPostRm: Path?,
+) {
+
+    companion object {
+        private const val permissionsRegular = "rw-r--r--"
+        private const val permissionsExecutable = "rwxr-xr-x"
+        internal val posixRegular = PosixFilePermissions.fromString(permissionsRegular)
+        internal val posixExecutable = PosixFilePermissions.fromString(permissionsExecutable)
+    }
+
+    private val logger: Logger = LoggerFactory.getLogger(AbstractDebPackager::class.java)
+    val debFileTree: Path = workingDir.resolve("debFileTree")
+
+    internal fun buildDebFileTree(appImage: Path, debFileTree: Path) {
+        val dirOpt = debFileTree.resolve("opt")
+        val dirPackage = dirOpt.resolve(linuxPackageName)
+        val dirBin = dirPackage.resolve("bin")
+        val dirLib = dirPackage.resolve("lib")
+        val dirShareDoc = dirPackage.resolve("share/doc/")
+        createDirectories(dirShareDoc, asFileAttribute(posixExecutable))
+        debCopyright?.copy(dirShareDoc.resolve("copyright"), posixRegular)
+        debLauncher?.copy(dirLib.resolve("$linuxPackageName-$packageName.desktop"), posixRegular)
+
+        syncDir(appImage.resolve("bin"), dirBin)
+        syncDir(appImage.resolve("lib"), dirLib) {
+            it != Paths.get("app/.jpackage.xml")
+        }
+    }
+
+    internal fun buildDebianDir(appImage: Path, debFileTree: Path) {
+        val dirDebian = debFileTree.resolve("DEBIAN")
+        dirDebian.createDirectories(asFileAttribute(posixExecutable))
+        val fileControl = dirDebian.resolve("control")
+        createControlFile(fileControl, appImage)
+        debPreInst?.copy(dirDebian.resolve("preinst"), posixExecutable)
+        debPostInst?.copy(dirDebian.resolve("postinst"), posixExecutable)
+        debPreRm?.copy(dirDebian.resolve("prerm"), posixExecutable)
+        debPostRm?.copy(dirDebian.resolve("postrm"), posixExecutable)
+    }
+
+    private fun Path.copy(target: Path, permissions: Set<PosixFilePermission>) {
+        target.parent.createDirectories(asFileAttribute(posixExecutable))
+        Files.copy(this, target)
+        setPosixFilePermissions(target, permissions)
+    }
+
+    internal fun createControlFile(fileControl: Path, appImage: Path) {
+        // Determine installed size as in jdk.jpackage.internal.LinuxDebBundler#createReplacementData()
+        val sizeInBytes = sizeInBytes(appImage)
+        val installedSize = (sizeInBytes shr 10).toString()
+        logger.info("size in bytes: $sizeInBytes")
+        logger.info("installed size: $installedSize")
+
+        val list = mutableListOf<String>().apply {
+            addAll(depends)
+        }
+        list.sort()
+
+        newBufferedWriter(fileControl).use { writer ->
+            writer.apply {
+                writeLn("Package: $linuxPackageName")
+                writeLn("Version: $packageVersion-1")
+                writeLn("Section: $appCategory")
+                writeLn("Maintainer: $packageVendor <$debMaintainer>")
+                writeLn("Priority: optional")
+                writeLn("Architecture: amd64")
+                writeLn("Provides: $linuxPackageName")
+                writeLn("Description: $packageDescription")
+                writeLn("Depends: ${list.joinToString(", ")}")
+                writeLn("Installed-Size: $installedSize")
+            }
+        }
+    }
+
+    // Same algorithm as jdk.jpackage.internal.PathGroup.Facade#sizeInBytes()
+    internal fun sizeInBytes(dir: Path): Long {
+        var sum: Long = 0
+        Files.walk(dir).use { stream ->
+            sum += stream.filter { p -> Files.isRegularFile(p) }
+                .mapToLong { f -> f.toFile().length() }.sum()
+        }
+        return sum
+    }
+
+    internal fun Writer.writeLn(s: String? = null) {
+        s?.let { write(it) }
+        write("\n")
+    }
+
+    internal fun syncDir(source: Path, target: Path, takeFile: (file: Path) -> Boolean = { _ -> true }) {
+        Files.walkFileTree(source, object : SimpleFileVisitor<Path>() {
+            override fun visitFile(file: Path, attrs: BasicFileAttributes): FileVisitResult {
+                val relative = source.relativize(file)
+                if (!takeFile(relative)) {
+                    return FileVisitResult.CONTINUE
+                }
+                val pathTarget = target.resolve(relative)
+                createDirectories(pathTarget.parent, asFileAttribute(posixExecutable))
+                if (Files.isExecutable(file)) {
+                    setPosixFilePermissions(file, posixExecutable)
+                } else {
+                    setPosixFilePermissions(file, posixRegular)
+                }
+                Files.copy(file, pathTarget)
+                return FileVisitResult.CONTINUE
+            }
+        })
+    }
+}
---
+++ pinpit/src/test/kotlin/de/mobanisto/pinpit/validation/deb/JvmDebPackager.kt
@@ -0,0 +1,177 @@
+/*
+ * Copyright 2022 Mobanisto UG (haftungsbeschraenkt) and respective authors and developers.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
+ */
+
+package de.mobanisto.pinpit.validation.deb
+
+import org.apache.commons.compress.archivers.ar.ArArchiveOutputStream
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream
+import org.apache.commons.compress.compressors.xz.XZCompressorOutputStream
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.nio.file.FileVisitResult
+import java.nio.file.Files
+import java.nio.file.Files.walkFileTree
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.nio.file.SimpleFileVisitor
+import java.nio.file.StandardCopyOption
+import java.nio.file.attribute.BasicFileAttributes
+import java.nio.file.attribute.PosixFilePermissions.asFileAttribute
+import kotlin.io.path.createDirectories
+import kotlin.io.path.isExecutable
+import kotlin.io.path.isRegularFile
+import kotlin.io.path.outputStream
+
+/**
+ * A class for creating a DEB package using without using native tools.
+ */
+class JvmDebPackager constructor(
+    private val appImage: Path,
+    private val destinationDir: Path,
+    workingDir: Path,
+    packageName: String,
+    private val linuxPackageName: String,
+    private val packageVersion: String,
+    private val arch: String,
+    appCategory: String,
+    packageVendor: String,
+    debMaintainer: String,
+    packageDescription: String,
+    depends: List<String>,
+    private val qualifier: String,
+    debCopyright: Path?,
+    debLauncher: Path?,
+    debPreInst: Path?,
+    debPostInst: Path?,
+    debPreRm: Path?,
+    debPostRm: Path?,
+) : AbstractDebPackager(
+    workingDir,
+    packageName,
+    linuxPackageName,
+    packageVersion,
+    appCategory,
+    packageVendor,
+    debMaintainer,
+    packageDescription,
+    depends,
+    debCopyright,
+    debLauncher,
+    debPreInst,
+    debPostInst,
+    debPreRm,
+    debPostRm
+) {
+
+    private val logger: Logger = LoggerFactory.getLogger(JvmDebPackager::class.java)
+    private val debPackageDir: Path = workingDir.resolve("debContent")
+
+    fun createPackage() {
+        logger.info("destination: $destinationDir")
+        destinationDir.createDirectories(asFileAttribute(posixExecutable))
+
+        logger.info("app image: $appImage")
+
+        logger.info("building debian file tree at: $debFileTree")
+        debFileTree.createDirectories(asFileAttribute(posixExecutable))
+
+        buildDebFileTree(appImage, debFileTree)
+        buildDebianDir(appImage, debFileTree)
+
+        logger.info("building debian archives at: $debPackageDir")
+        debPackageDir.createDirectories(asFileAttribute(posixExecutable))
+
+        val deb = destinationDir.resolve("$linuxPackageName-$qualifier-$arch-$packageVersion.deb")
+        packageDeb(deb, debPackageDir, debFileTree)
+    }
+
+    private fun packageDeb(deb: Path, debPackageDir: Path, debFileTree: Path) {
+        val nameControl = "control.tar.xz"
+        val nameData = "data.tar.xz"
+
+        val fileControl = debPackageDir.resolve(nameControl)
+        val fileData = debPackageDir.resolve(nameData)
+
+        packageControl(fileControl, debFileTree)
+        packageData(fileData, debFileTree)
+
+        deb.outputStream().use { fos ->
+            ArArchiveOutputStream(fos).use { ar ->
+                val entryControl = ar.createArchiveEntry(fileControl, nameControl)
+                ar.putArchiveEntry(entryControl)
+                Files.copy(fileControl, ar)
+                ar.closeArchiveEntry()
+
+                val entryData = ar.createArchiveEntry(fileData, nameData)
+                ar.putArchiveEntry(entryData)
+                Files.copy(fileData, ar)
+                ar.closeArchiveEntry()
+            }
+        }
+
+        Files.copy(fileControl, Paths.get("/tmp/control.tar.xz"), StandardCopyOption.REPLACE_EXISTING)
+        Files.copy(fileData, Paths.get("/tmp/data.tar.xz"), StandardCopyOption.REPLACE_EXISTING)
+        Files.copy(deb, Paths.get("/tmp/file.deb"), StandardCopyOption.REPLACE_EXISTING)
+    }
+
+    private fun packageControl(fileControl: Path, debFileTree: Path) {
+        val debian = debFileTree.resolve("DEBIAN")
+        packageTarXz(fileControl) {
+            walkFileTree(debian, object : SimpleFileVisitor<Path>() {
+                override fun preVisitDirectory(dir: Path, attrs: BasicFileAttributes): FileVisitResult {
+                    packageFile(debian, dir)
+                    return FileVisitResult.CONTINUE
+                }
+
+                override fun visitFile(file: Path, attrs: BasicFileAttributes): FileVisitResult {
+                    packageFile(debian, file)
+                    return FileVisitResult.CONTINUE
+                }
+            })
+        }
+    }
+
+    private fun packageData(fileData: Path, debFileTree: Path) {
+        packageTarXz(fileData) {
+            walkFileTree(debFileTree, object : SimpleFileVisitor<Path>() {
+                override fun preVisitDirectory(dir: Path, attrs: BasicFileAttributes): FileVisitResult {
+                    val relative = debFileTree.relativize(dir)
+                    if (relative == Paths.get("DEBIAN")) return FileVisitResult.SKIP_SUBTREE
+                    packageFile(debFileTree, dir)
+                    return FileVisitResult.CONTINUE
+                }
+
+                override fun visitFile(file: Path, attrs: BasicFileAttributes): FileVisitResult {
+                    packageFile(debFileTree, file)
+                    return FileVisitResult.CONTINUE
+                }
+            })
+        }
+    }
+
+    private fun TarArchiveOutputStream.packageFile(dir: Path, file: Path) {
+        val relative = dir.relativize(file)
+        val entry = createArchiveEntry(file, "./$relative") as TarArchiveEntry
+        entry.userId = 0
+        entry.groupId = 0
+        entry.mode = (if (file.isExecutable()) "755" else "644").toInt(radix = 8)
+        putArchiveEntry(entry)
+        if (file.isRegularFile()) {
+            Files.copy(file, this)
+        }
+        closeArchiveEntry()
+    }
+
+    private fun packageTarXz(outputFile: Path, fn: TarArchiveOutputStream.() -> Unit) {
+        outputFile.outputStream().use { fos ->
+            XZCompressorOutputStream(fos).use { xz ->
+                TarArchiveOutputStream(xz).use { tar ->
+                    tar.fn()
+                }
+            }
+        }
+    }
+}
